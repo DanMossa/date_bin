0c54570371914b387331c8e532ea7dc0d4a19d4a
Daniel Mossaband <DanMossa@gmail.com>
2023-04-09 16:07:48 -0700

    First commit

---
+++ example/date_bin_example.dart
@@ -0,0 +1,6 @@
+import 'package:date_bin/date_bin.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/date_bin.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library date_bin;
+
+export 'src/date_bin_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
